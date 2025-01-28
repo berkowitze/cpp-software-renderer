3d4f7fef5bb2a0cc9ac7c2985c1e0c8222fb0b2e
Eli Berkowitz <eliberkowitz@gmail.com>
2025-01-28 15:21:16 -0500

    line drawing done

--- main.cpp
+++ main.cpp
@@ -7,61 +7,74 @@ const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void draw_line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
-    int dx = x1 - x0;
-    int dy = y1 - y0;
-
     // The line is "steep" if it changes more in y than in x
     // This can be used to make sure that lines are drawn without holes
     // and also with as few iterations as needed.
-    bool is_steep = (std::abs(dx) < std::abs(dy));
+    bool is_steep = std::abs(x0 - x1) < std::abs(y0 - y1);
 
-    // If the line is steep, we want to iterate over y to avoid holes
-    // Rather than branching, we'll just swap x and y here.
     if (is_steep)
     {
+        // if the line is steep, we're going to reflect it over y=x so we can treat it
+        // like a non-steep line and always iterate over "x". Then we'll have to untranspose it
+        // with x=y when actually writing to the image.
+        // We can also use the fact that the slope is less than 1 to our advantage.
         std::swap(x0, y0);
         std::swap(x1, y1);
     }
 
-    // If the first input point is to the right of (or above if is_steep) the second,
-    // swap the point order so that it's always correct to increment positively
+    // Make it always left-to-right
     if (x0 > x1)
     {
         std::swap(x0, x1);
         std::swap(y0, y1);
     }
 
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+
+    /*  less efficient approach since you have to do floating point stuff
+    // derror (Delta Error) is how much error accumulates with each iteration of x
+    // error is really just how much y changes as x changes (abs slope), I'm just calling it "error"
+    // to be consistent with the tutorial
+    // float derror = std::abs(dy / (float)dx);
+    */
+    // instead of doing error = dy/dx, let's do new_error = 2 * dy = 2 * dx * old_error
+    int dxderror2 = std::abs(dy) * 2;
+    // error is how much cumulative error has accumulated
     int error = 0;
+    int y_increment = (y1 > y0) ? 1 : -1;
+
+    int y = y0;
+    // This could be a single loop with an if statement, but branching inside
     if (is_steep)
     {
         for (int x = x0; x <= x1; x++)
         {
-            //  In this case, y and x were transposed, so untranspose them here
-
             image.set(y, x, color);
+
+            error += dxderror2;
+            if (error > dx)
+            {
+                // if the cumulative error is over 0.5, we want to move y up/down to the next pixel
+                y += y_increment;
+                error -= dx * 2;
+            }
         }
     }
-
-    // Precompute to save time
-    // double divisor = 1.0 / (x1 - x0);
-
-    // // x isn't always on the x axis, it is on the y axis if `is_steep`
-    // for (int x = x0; x <= x1; x++)
-    // {
-    //     // t is how far along the line (from 0 to 1) we are
-    //     double t = (x - x0) * divisor;
-    //     // Lerp y from y0 to y1 based on t
-    //     int y = y0 * (1.0 - t) + y1 * t;
-
-    //     if (is_steep)
-    //     {
-    //         image.set(y, x, color);
-    //     }
-    //     else
-    //     {
-    //         image.set(x, y, color);
-    //     }
-    // }
+    else
+    {
+        for (int x = x0; x <= x1; x++)
+        {
+            image.set(x, y, color);
+            error += dxderror2;
+            if (error > dx)
+            {
+                // if the cumulative error is over 0.5, we want to move y up/down to the next pixel
+                y += y_increment;
+                error -= dx * 2;
+            }
+        }
+    }
 }
 
 int main(int argc, char **argv)
@@ -69,14 +82,16 @@ int main(int argc, char **argv)
     TGAImage image(100, 100, TGAImage::RGB);
     image.set(52, 41, red);
     // normal line, is_steep
-    draw_line(10, 20, 40, 70, image, white);
+    draw_line(10, 10, 50, 50, image, white);
     // normal line, !is_steep
     draw_line(10, 50, 40, 70, image, TGAColor(255, 255, 0, 255));
-    // horizontal line
+    // horizontal lines
     draw_line(10, 30, 40, 30, image, TGAColor(255, 0, 255, 255));
-    // vertical line
-    draw_line(60, 40, 60, 20, image, TGAColor(0, 255, 255, 255));
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    draw_line(40, 40, 10, 40, image, TGAColor(255, 100, 255, 255));
+    // vertical lines
+    draw_line(60, 40, 60, 20, image, TGAColor(255, 100, 100, 255));
+    draw_line(70, 20, 70, 40, image, TGAColor(255, 0, 0, 255));
+    image.flip_vertically();
     image.write_tga_file("output.tga");
     return 0;
 }
